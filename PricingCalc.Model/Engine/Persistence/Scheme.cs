8cf6cddc88c93a471f2ce9b5a73349b0df8c3d49
AlexNav73 <alexandr.navitskiy@gmail.com>
2021-06-29 23:27:12 +0200

    linked all TODOs to GitHub issues

--- PricingCalc.Model/Engine/Persistence/Scheme.cs
+++ PricingCalc.Model/Engine/Persistence/Scheme.cs
@@ -5,6 +5,6 @@ namespace PricingCalc.Model.Engine.Persistence
 {
     public record Property(string Name, Type Type, bool IsNullable);
 
-    // TODO: Could we use PropertiesBag to generate queries?
+    // TODO(#6): Could we use PropertiesBag to generate queries?
     public record Scheme(IList<Property> Properties);
 }
