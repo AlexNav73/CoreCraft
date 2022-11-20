3b3c84e4969d44d827df89651b4a281fdf65386f
AlexNav73 <alexandr.navitskiy@gmail.com>
2022-11-20 13:14:11 +0100

    added another snapshot test

--- src/Tests/Navitski.Crystalized.Model.Generators.Tests/ApplicationModelGeneratorTests.cs
+++ src/Tests/Navitski.Crystalized.Model.Generators.Tests/ApplicationModelGeneratorTests.cs
@@ -7,10 +7,26 @@ namespace Navitski.Crystalized.Model.Generators.Tests
         [Test]
         public Task ModelWithAllFeaturesTest()
         {
-            return Run(files: "AllFeatures.model.json", verification: result =>
+            return Run(verification: result =>
             {
                 Assert.That(result, Is.Not.Null);
                 Assert.That(result.Diagnostics.Count(), Is.EqualTo(0));
+            },
+            files: "AllFeatures.model.json");
+        }
+
+        [Test]
+        public Task TwoModelFilesTest()
+        {
+            return Run(verification: result =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Diagnostics.Count(), Is.EqualTo(0));
+            },
+            files: new[]
+            {
+                "AllFeatures.model.json",
+                "AnotherModelFile.model.json"
             });
         }
     }
