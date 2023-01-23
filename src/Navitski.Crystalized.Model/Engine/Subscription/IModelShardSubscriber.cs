65628408c2dd67d529b2c347207eef36190c5ef7
AlexNav73 <alexandr.navitskiy@gmail.com>
2023-01-23 17:39:51 +0100

    use the latest c# features thx to PolySharp

--- src/Navitski.Crystalized.Model/Engine/Subscription/IModelShardSubscriber.cs
+++ src/Navitski.Crystalized.Model/Engine/Subscription/IModelShardSubscriber.cs
@@ -1,9 +1,5 @@
 ﻿using Navitski.Crystalized.Model.Engine.ChangesTracking;
-#if NETSTANDARD2_0_OR_GREATER
-using System.Linq.Expressions;
-#elif NET5_0_OR_GREATER
 using System.Runtime.CompilerServices;
-#endif
 
 namespace Navitski.Crystalized.Model.Engine.Subscription;
 
@@ -14,7 +10,6 @@ namespace Navitski.Crystalized.Model.Engine.Subscription;
 public interface IModelShardSubscriber<T> : ISubscriber<T>
     where T : class, IChangesFrame
 {
-#if NET5_0_OR_GREATER
     /// <summary>
     ///     Choose a collection to receive it's changes
     /// </summary>
@@ -42,30 +37,4 @@ public interface IModelShardSubscriber<T> : ISubscriber<T>
         [CallerArgumentExpression("accessor")] string expression = "")
         where TParent : Entity
         where TChild : Entity;
-
-#elif NETSTANDARD2_0_OR_GREATER
-    /// <summary>
-    ///     Choose a collection to receive it's changes
-    /// </summary>
-    /// <typeparam name="TEntity">An entity type</typeparam>
-    /// <typeparam name="TProperties">A type of a properties</typeparam>
-    /// <param name="accessor">An expression of a collection property</param>
-    /// <returns>A subscriber for the collection changes</returns>
-    ICollectionSubscriber<TEntity, TProperties> With<TEntity, TProperties>(
-        Expression<Func<T, ICollectionChangeSet<TEntity, TProperties>>> accessor)
-        where TEntity : Entity
-        where TProperties : Properties;
-
-    /// <summary>
-    ///     Choose a relation to receive it's changes
-    /// </summary>
-    /// <typeparam name="TParent">A type of parent entity</typeparam>
-    /// <typeparam name="TChild">A type of child entity</typeparam>
-    /// <param name="accessor">An expression of a relation property</param>
-    /// <returns>A subscriber for the relation changes</returns>
-    IRelationSubscriber<TParent, TChild> With<TParent, TChild>(
-        Expression<Func<T, IRelationChangeSet<TParent, TChild>>> accessor)
-        where TParent : Entity
-        where TChild : Entity;
-#endif
 }
--- src/Navitski.Crystalized.Model/Engine/Subscription/ModelShardSubscriber.cs
+++ src/Navitski.Crystalized.Model/Engine/Subscription/ModelShardSubscriber.cs
@@ -1,10 +1,5 @@
 ﻿using Navitski.Crystalized.Model.Engine.ChangesTracking;
-#if NETSTANDARD2_0_OR_GREATER
-using Navitski.Crystalized.Model.Engine.Exceptions;
-using System.Linq.Expressions;
-#elif NET5_0_OR_GREATER
 using System.Runtime.CompilerServices;
-#endif
 
 namespace Navitski.Crystalized.Model.Engine.Subscription;
 
@@ -18,7 +13,6 @@ internal sealed class ModelShardSubscriber<T> : Subscriber<T>, IModelShardSubscr
         _subscriptions = new Dictionary<string, ISubscription<T>>();
     }
 
-#if NET5_0_OR_GREATER
     public ICollectionSubscriber<TEntity, TProperties> With<TEntity, TProperties>(
         Func<T, ICollectionChangeSet<TEntity, TProperties>> accessor,
         [CallerArgumentExpression(nameof(accessor))] string expression = "")
@@ -52,50 +46,6 @@ internal sealed class ModelShardSubscriber<T> : Subscriber<T>, IModelShardSubscr
 
         return subscriber;
     }
-
-#elif NETSTANDARD2_0_OR_GREATER
-    public ICollectionSubscriber<TEntity, TProperties> With<TEntity, TProperties>(
-        Expression<Func<T, ICollectionChangeSet<TEntity, TProperties>>> accessor)
-        where TEntity : Entity
-        where TProperties : Properties
-    {
-        if (accessor.Body is MemberExpression memberExpression)
-        {
-            if (_subscriptions.TryGetValue(memberExpression.Member.Name, out var subs))
-            {
-                return (ICollectionSubscriber<TEntity, TProperties>)subs;
-            }
-
-            var subscriber = new CollectionSubscriber<T, TEntity, TProperties>(accessor.Compile());
-            _subscriptions.Add(memberExpression.Member.Name, subscriber);
-
-            return subscriber;
-        }
-
-        throw new InvalidPropertySubscriptionException("Accessor should contain only property access");
-    }
-
-    public IRelationSubscriber<TParent, TChild> With<TParent, TChild>(
-        Expression<Func<T, IRelationChangeSet<TParent, TChild>>> accessor)
-        where TParent : Entity
-        where TChild : Entity
-    {
-        if (accessor.Body is MemberExpression memberExpression)
-        {
-            if (_subscriptions.TryGetValue(memberExpression.Member.Name, out var subs))
-            {
-                return (IRelationSubscriber<TParent, TChild>)subs;
-            }
-
-            var subscriber = new RelationSubscriber<T, TParent, TChild>(accessor.Compile());
-            _subscriptions.Add(memberExpression.Member.Name, subscriber);
-
-            return subscriber;
-        }
-
-        throw new InvalidPropertySubscriptionException("Accessor should contain only property access");
-    }
-#endif
 
     public void Publish(Change<IModelChanges> change)
     {
